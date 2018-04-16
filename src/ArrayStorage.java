7c01226b6659aba85d657e7b48a93765b1e470b6
Denis Wolf <i-wild-wolf@yandex.ru>
2018-04-16 15:52:48 +0300

    Created minimal functionality for ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,28 +3,42 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private static int counter = 0;
 
     void clear() {
+        for (int i = 0; i < counter; i++)
+            storage[i] = null;
+        counter = 0;
     }
 
     void save(Resume r) {
+        storage[counter] = r;
+        counter++;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < counter; i++)
+            if(storage[i].uuid.equals(uuid))
+                return storage[i];
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < counter; i++)
+            if(storage[i].uuid.equals(uuid))
+                storage[i] = storage[counter--];
+
+
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return new Resume[counter];
     }
 
     int size() {
-        return 0;
+        return counter;
     }
 }
